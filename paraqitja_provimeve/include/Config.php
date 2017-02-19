752f77771b0d505997c7256485553cd53fdb191a
besjonaraqi <besjonaraqi@gmail.com>
2017-02-19 15:05:05 +0100

    Update Config.php

--- paraqitja_provimeve/include/Config.php
+++ paraqitja_provimeve/include/Config.php
@@ -2,6 +2,6 @@
  
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASSWORD", "besjonaraqi");
-define("DB_DATABASE", "db_paraqitja_provimeve");
+define("DB_PASSWORD", "password");
+define("DB_DATABASE", "database");
 ?>
